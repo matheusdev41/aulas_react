698b23446a3fc768d2cc4fddecd64e21fab1392b
Matheus Pessoa <matheus41.dev@gmail.com>
2024-07-14 13:24:49 -0300

    aulas-react

---
+++ projeto2/src/Titulo.jsx
@@ -0,0 +1,11 @@
+function Titulo() {
+    
+    return(
+        <div>
+            <h1>Oi eu sou um título</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti accusantium ex necessitatibus aperiam, repellat mollitia placeat, voluptate asperiores non impedit veritatis at perspiciatis delectus atque voluptatem laudantium optio rerum tempore!</p>
+        </div>
+    )
+}
+
+export default Titulo
